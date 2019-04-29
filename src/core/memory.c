27b2d2cf10d91c5c40bd4470a193e1a00763b759
Hiroshi123 <vrodxda@gmail.com>
2019-04-29 23:11:37 +0900

    add simple loader

--- src/core/memory.c
+++ src/core/memory.c
@@ -13,15 +13,15 @@
 
 // static char COUNT = 0;
 
-static heap *HEAP_HEADER_ADDR_HEAD;
-static heap *HEAP_HEADER_ADDR_P;
-static heap *HEAP_HEADER_ADDR_TAIL;
-static size_t PAGE_SIZE;
+/*static*/ heap *HEAP_HEADER_ADDR_HEAD;
+heap *HEAP_HEADER_ADDR_P;
+heap *HEAP_HEADER_ADDR_TAIL;
+size_t PAGE_SIZE;
 
 __attribute__((constructor)) void set_heap_header() {
   PAGE_SIZE = getpagesize();
   heap *tmp = (heap*)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
-                   MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (tmp == MAP_FAILED) {
     printf("error\n");
     exit(0);
@@ -32,6 +32,7 @@ __attribute__((constructor)) void set_heap_header() {
 }
 
 __attribute__((destructor)) void unset_heap_header() {
+  printf("unset:%x\n");
   const heap *h = (heap *)HEAP_HEADER_ADDR_HEAD;
   for (; h != HEAP_HEADER_ADDR_P; h++) {
     munmap(h->begin, /*h->page_num * */PAGE_SIZE);
@@ -50,6 +51,7 @@ heap *init_map_file(const char *const fname) {
     return 0;
   }
   const size_t map_size = ((stbuf.st_size + 0x1000) & 0xfffff000);
+  printf("map:%x,%x\n",stbuf.st_size,map_size);
   void *begin = mmap(NULL, map_size, PROT_READ|PROT_WRITE|PROT_EXEC,
                      MAP_PRIVATE, fd, 0);
   if (begin == MAP_FAILED) {
@@ -61,12 +63,12 @@ heap *init_map_file(const char *const fname) {
   h->begin = begin;
   h->page_num = (uint16_t)0x1000;
   h->page_num += stbuf.st_size;
+  h->guest_addr = -1;
   // TODO.
   // PROT & MAP flags for mmap is scheduled to be condensed as 4byte.
   // Although it is originally 16byte(8+8)
   h->flags = 1;
   h->file = (uint16_t)fd;
-  char* aa = h+1;
   HEAP_HEADER_ADDR_P += 1;
   return h;
 }
@@ -82,11 +84,12 @@ heap* get_page(uint8_t num) {
   h->page_num = map_size / PAGE_SIZE;
   h->flags = 1;
   h->file = (uint16_t)fd;
+  h->guest_addr = -1;
   HEAP_HEADER_ADDR_P = h + 1;  
   return h;
 }
 
-char* get_page_head() {
+uint8_t* get_page_head() {
   heap* h = (heap*)(HEAP_HEADER_ADDR_P-1);
   return h->begin;
 }
@@ -94,3 +97,50 @@ char* get_page_head() {
 heap* get_current_meta_addr() {
   return (heap*)HEAP_HEADER_ADDR_P;
 }
+
+heap* get_current_meta_head() {
+  printf("meta:%x,%x\n",HEAP_HEADER_ADDR_HEAD,*(uint8_t*)HEAP_HEADER_ADDR_HEAD);
+  return (heap*)HEAP_HEADER_ADDR_HEAD;
+}
+
+heap* guest_mmap(void* guest_addr, uint8_t page_num) {
+  
+  //
+  int map_size = PAGE_SIZE*page_num;
+  int fd = -1;
+  void *begin = mmap(NULL,map_size , PROT_READ|PROT_WRITE|PROT_EXEC,
+                     MAP_PRIVATE|MAP_ANONYMOUS, fd, 0);
+  heap *h = (heap *)HEAP_HEADER_ADDR_P;
+  printf("begin:%x,%x\n",begin, h);
+  h->begin = begin;
+  h->page_num = map_size / PAGE_SIZE;
+  h->flags = 1;
+  h->file = (uint16_t)fd;
+  h->guest_addr = guest_addr;
+  HEAP_HEADER_ADDR_P += 1;  
+  return h;
+}
+
+void* get_diff_host_guest_addr(void* guest_addr) {
+
+  printf("arg1:%x\n",guest_addr);
+  
+  heap* h = HEAP_HEADER_ADDR_HEAD;
+  heap* h_end = HEAP_HEADER_ADDR_P;
+  void* guest_begin = (uint64_t)guest_addr & 0xfffff000;
+  for (;h!=h_end;h++) {
+    printf("%x\n",h->begin);
+    if (h->guest_addr != -1) {
+      printf("g:%x,%x\n",h->guest_addr,guest_begin);
+      if (guest_begin == h->guest_addr) {
+	uint64_t diff = (uint64_t)h->begin - (uint64_t)guest_begin;
+	printf("diff:%x,%x\n",h->begin,diff);
+	return (void*)diff;
+      }
+      /* if (h->guest_addr != h->page_num) {	 */
+      /* } */
+    }
+  }
+  return NULL;
+}
+
--- src/core/pe.c
+++ src/core/pe.c
@@ -1,6 +1,122 @@
 
 #include "pe.h"
+#include "memory.h"
+
 
 const char check_pe(const uint16_t* p) {
-  return p == 0;
+  
+  return *p == 0x5a4d;
 }
+
+char load_pe(const char *const begin,void* stack_addr
+	     /*info_on_pe *e*/) {
+  
+  IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)begin;
+  printf("%x,%x\n",dos_header->e_magic,dos_header->e_lfanew);
+  uint16_t* v = (uint16_t*)((uint8_t*)dos_header + dos_header->e_lfanew + 0x18);
+  printf("%x,%x\n",v,*v);
+  if (*v == 0x10b) {
+    load_pe32(dos_header, stack_addr);
+  } else if (*v == 0x20b) {
+    load_pe64(dos_header, stack_addr);
+  }
+}
+
+void load_pe32(IMAGE_DOS_HEADER* p_dos_header, void* stack_addr) {
+  
+  IMAGE_NT_HEADERS32* nt_header32 = (IMAGE_NT_HEADERS32*)((uint8_t*)p_dos_header + p_dos_header->e_lfanew);
+  printf("!%x,%d\n",&nt_header32->OptionalHeader.Magic,sizeof(nt_header32));
+  
+}
+
+void load_pe64(IMAGE_DOS_HEADER* p_dos_header, void* stack_addr) {
+  
+  IMAGE_NT_HEADERS64* nt_header = (IMAGE_NT_HEADERS64*)((uint8_t*)p_dos_header + p_dos_header->e_lfanew);
+  printf("!!%x,%x\n",nt_header->OptionalHeader.BaseOfCode ,nt_header->OptionalHeader.AddressOfEntryPoint);
+  
+  printf("num sec %d\n", nt_header->FileHeader.NumberOfSections);
+  printf("num sec %d\n", nt_header->FileHeader.NumberOfSymbols);
+  
+  printf("%x\n", nt_header->OptionalHeader.BaseOfCode);
+  
+  printf("%x\n", nt_header->OptionalHeader.SizeOfCode);
+  printf("%d\n", nt_header->OptionalHeader.SizeOfUninitializedData);
+  printf("%d\n", nt_header->OptionalHeader.SizeOfImage);
+  
+  printf("0,%d\n", nt_header->OptionalHeader.DataDirectory[0].Size);
+  printf("1,%d\n", nt_header->OptionalHeader.DataDirectory[1].Size);
+  printf("2,%d\n", nt_header->OptionalHeader.DataDirectory[2].Size);
+  printf("3,%d\n", nt_header->OptionalHeader.DataDirectory[3].Size);
+  printf("4,%d\n", nt_header->OptionalHeader.DataDirectory[4].Size);
+  printf("5,%d\n", nt_header->OptionalHeader.DataDirectory[5].Size);
+  printf("6,%d\n", nt_header->OptionalHeader.DataDirectory[6].Size);
+  printf("7,%d\n", nt_header->OptionalHeader.DataDirectory[7].Size);
+  printf("8,%d\n", nt_header->OptionalHeader.DataDirectory[8].Size);
+  printf("9,%d\n", nt_header->OptionalHeader.DataDirectory[9].Size);
+  printf("10,%d\n", nt_header->OptionalHeader.DataDirectory[10].Size);
+  printf("11,%d\n", nt_header->OptionalHeader.DataDirectory[11].Size);
+  printf("12,%d\n", nt_header->OptionalHeader.DataDirectory[12].Size);
+  printf("13,%d\n", nt_header->OptionalHeader.DataDirectory[13].Size);
+  printf("14,%d\n", nt_header->OptionalHeader.DataDirectory[14].Size);
+  
+  uint64_t* a = ((uint8_t*)(p_dos_header)) /*+ nt_header->OptionalHeader.ImageBase*/
+    + nt_header->OptionalHeader.BaseOfCode;
+  
+  printf("cdc,%x,%x\n",a,*(a));
+  
+  IMAGE_SECTION_HEADER* s = (IMAGE_SECTION_HEADER*)(nt_header + 1);
+  printf("%x\n",s);
+  
+  /* IMAGE_SECTION_HEADER* s = e->sec_begin; */
+  heap* h;
+  
+  // mapping image header(dos header + file feader + optional header) itself
+  h = guest_mmap(nt_header->OptionalHeader.ImageBase,1);
+  memcpy
+    (h->begin,
+     p_dos_header,
+     (uint64_t)s - (uint64_t)p_dos_header
+     );
+  // mmap section header.
+  int sec_num = nt_header->FileHeader.NumberOfSections;  
+  IMAGE_SECTION_HEADER* end = s + sec_num;
+  for (;s!=end;s++) {
+    // guest map also have to have some flags...
+    h = guest_mmap(nt_header->OptionalHeader.ImageBase + s->VirtualAddress, 1);
+    printf("p:%x,%x\n",h->begin, h->page_num);
+    void* dst = memcpy
+      (h->begin,
+       (uint8_t*)p_dos_header + s->PointerToRawData,
+       s->SizeOfRawData);
+    printf("dst:%x\n",dst);
+    /* printf("%x,%x\n",h,h->begin); */    
+    // memcpy(s->VirtualAddress, );
+    // s->PointerToRawData    
+    printf("!%x\n",s);
+    printf("%s\n",s->Name);
+    printf("%x\n", s->NumberOfRelocations);
+    printf("%x\n", s->PointerToRelocations);
+    printf("%x\n", s->PointerToRawData);
+    printf("%x\n", s->SizeOfRawData);
+    printf("%x\n", s->VirtualAddress);
+    printf("%x\n", s->Characteristics);
+    printf("---\n");
+  }
+  
+  // stack Reserved
+  void* stack_head = (uint64_t)stack_addr & 0xfffff000;
+  h = guest_mmap(stack_head, 1); 
+  // heap Reserve
+  void* start_addr = nt_header->OptionalHeader.ImageBase + nt_header->OptionalHeader.AddressOfEntryPoint;
+  return start_addr;
+}
+
+// prepare host/get address translation mapping.
+// guest (page1) 0x40000000 -> host current address();
+// guest (page2) 0x
+
+// offset calculation
+// 0x40000000
+// 0x40001000
+// 
+
