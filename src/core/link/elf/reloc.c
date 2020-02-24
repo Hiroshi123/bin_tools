716735f7e688fce04ed15691bc02449e47299273
hiroshi123 <vrodxda@gmail.com>
2020-02-24 22:41:38 +0900

    add better reloc

--- src/core/link/elf/reloc.c
+++ src/core/link/elf/reloc.c
@@ -73,7 +73,7 @@ static uint32_t resolve_external(char* name) {
   }
 }
 
-static void _resolve(ObjectChain* oc, Elf64_Rela* rela, void* addr) {
+static void resolve(ObjectChain* oc, Elf64_Rela* rela, void* addr) {
   Elf64_Sym* sym = oc->symbol_table_p;
   int num = oc->symbol_num;
   printf("%p,%p,%p\n", sym, num, ELF64_R_SYM(rela->r_info));
@@ -93,58 +93,7 @@ static void _resolve(ObjectChain* oc, Elf64_Rela* rela, void* addr) {
   }
 }
 
-static void resolve(uint8_t* p, Elf64_Rela* rela, void* addr) {
-
-  sh_callback_arg* _p = p;
-  Elf64_Sym* sym = _p->symbol_offset;
-  int size = _p->symbol_size;
-  int i = 0;
-  int secndx = -1;
-  /* Elf64_Sym* s = sym; */
-  Elf64_Sym* end = sym + size;  
-  for (;sym!=end;sym++,i++) {
-    if (i == ELF64_R_SYM(rela->r_info)) {
-      switch (ELF64_ST_TYPE(sym->st_info)) {
-      case STT_NOTYPE:
-	printf("this is no type,%p\n", rela->r_addend);
-	// resolve_external();
-	break;
-      case STT_SECTION:
-	secndx = sym->st_shndx;	
-	break;
-      }
-      continue;
-    }
-    // if section index equals
-    if (sym->st_shndx == secndx && sym->st_value == rela->r_addend) {
-      printf("matched\n");
-      break;
-    }
-    // printf("!!%p,%d\n", sym, sym->st_value);    
-  }
-  Elf64_Shdr* shdr;
-  if (secndx == -1) {
-    printf("should emit error\n");
-  }
-  // _p->section_head + secndx;
-  shdr = ((Elf64_Shdr*)_p->section_head) + secndx;
-  printf("addr:%p\n", shdr->sh_addr + rela->r_addend);
-  // should reflect type of relocation..
-  // R_X86_64_32S
-  switch (ELF64_R_TYPE(rela->r_info)) {
-  case R_X86_64_64:
-    *((uint64_t*)addr) = shdr->sh_addr + rela->r_addend;
-    break;
-  case R_X86_64_32S:
-    *((uint32_t*)addr) = shdr->sh_addr + rela->r_addend;
-    printf("this type!!!,%p,%p\n", addr, *((uint32_t*)addr-1));
-    break;
-  default:
-    break;
-  }
-}
-
-void _do_reloc(void* _oc, void* arg1) {
+void do_reloc(void* _oc, void* arg1) {
 
   ObjectChain* oc = _oc;// Confp->initial_object;
   // Confp->initial_object;
@@ -161,33 +110,9 @@ void _do_reloc(void* _oc, void* arg1) {
     for (;rela<rela_end;rela++) {
       printf("r:%p\n", rela);
       addr = shdr->sh_offset + rela->r_offset;
-      _resolve(oc, rela, addr);
+      resolve(oc, rela, addr);
     }
   }
 }
 
-void do_reloc (void* p) {
-  
-  sh_callback_arg* _p = p;  
-  Elf64_Rela* rela = _p->rel_offset;
-  Elf64_Rela* rela_end = (uint8_t*)rela + _p->rel_size;
-  Elf64_Shdr* shdr;
-  for (;rela<rela_end;rela++) {
-    printf("r:%p\n", rela);
-    printf("rela offset :%p\n", rela->r_offset);
-    printf("rela info(sym) :%p\n", ELF64_R_SYM(rela->r_info));
-    printf("rela info(type) :%p\n", ELF64_R_TYPE(rela->r_info));
-    printf("rela addend :%p\n", rela->r_addend);
-    // if (ELF64_R_TYPE(rela->r_info) == R_X86_64_64/*STV_INTERNAL*/) {
-    printf("rel sec : %p\n", _p->rel_sec);
-    shdr = _p->rel_sec;
-    printf("sh offset:%p,%p\n", shdr->sh_offset, _p->symbol_offset);
-    uint8_t* addr = shdr->sh_offset + rela->r_offset;      
-    resolve(_p, rela, addr);
-    printf("resolved address:%p,%p,%p\n", addr, *addr, *(addr - 2));
-    // ELF64_R_SYM(rela->r_info)
-    // }
-  }
-  printf("do reloc,%d,%p\n",_p->rel_secndx, _p->rel_sec);
-}
 
