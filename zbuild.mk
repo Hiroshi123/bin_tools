c41405142b6814213b2770b7ade35fba74ba8b2c
hiroshi123 <vrodxda@gmail.com>
2020-06-25 21:25:15 +0900

    check cause of segfault

--- zbuild.mk
+++ zbuild.mk
@@ -60,7 +60,7 @@ $(sodir)/__z__os.so : $(objdir)/x64_syscall.o
 	./zld $^ -o $@
 
 $(objdir)/x64_syscall.o : $(coredir)/os/linux/x64_syscall.asm
-	nasm -felf64 $^ -v -o $@
+	nasm -felf64 $^ -o $@
 
 # create logger
 
