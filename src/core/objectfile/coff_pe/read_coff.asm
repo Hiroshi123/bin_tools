61cdb86f3cdfff909d31cddb4efce7cf9f8cf527
Hiroshi123 <vrodxda@gmail.com>
2020-07-16 08:30:35 +0900

    add missing files for build

--- src/core/objectfile/coff_pe/read_coff.asm
+++ src/core/objectfile/coff_pe/read_coff.asm
@@ -1,29 +1,75 @@
 
 	default rel
 	section .text
-	global run_through_coff_shdr3
-	global run_through_coff_symtable3
+	global __z__obj__run_through_coff_shdr3
+	global __z__obj__run_through_coff_symtable3
 
-run_through_coff_shdr3:
+;;; microsoft
+;;; caller saved register RAX, RCX, RDX, R8, R9, R10, R11
+;;; callee saved register RBX, RBP, RDI, RSI, RSP, R12, R13, R14, and R15
 
+;;; sysv args
+;; RDI, RSI, RDX, RCX, R8, R9
+	
+__z__obj__run_through_coff_shdr3:
+	
 	push rbp
 	mov rbp,rsp
 	push rbx
-	push r12
-	;; push r13
-	;; push r14
-	;; push r15
-	;; ;; arguments are preserved on a stack.
-	;; push rdi
-	;; push rsi
-	;; push rdx
-	
-	mov rax,rdx
-	mov rdx,rcx
-	add rdx,0x14
-	call rax
-	mov rax,3
+	push rdx
 
+	;; 2nd(rsi) should be set on r13(which is one of registers kept on caller)
+	mov r13,rsi
+	;; add number of sections
+	mov rax,0
+	mov word ax,[rdi+2]
+	mov r15,rax
+	cmp r15,0
+	je __z__obj__run_through_coff_shdr3._b2
+	;; r12 = string table pointer (end of symbol table)
+	;; get End of SymbolTable which is beginning of string table
+
+	;; get NumberOfSymbols
+	;; mov dword eax,[rdi+0xc]
+	;; mov r14,rax
+	;; mov rax,0x12
+	;; mul r14
+	;; ;; get PointerToSymbolTable(+0x8)
+	;; mov r12,rax
+	;; mov dword eax,[rdi+0x8]
+	;; add r12,rax
+	;; add r12,rdi
+	mov r12,rdi
+	
+	;; 1st argument is the top of file pointer
+	mov r14,rdi
+	add r14,0x14
+	;; add size of optional header
+	mov rax,0
+	mov word ax,[rdi+0x10]
+	add r14,rax
+
+	;; preserve rdx(3rd argument) as it is volatile
+	;; mov rbx,rcx
+	
+._b1:
+	;; 1st arg
+	mov rdi,r14
+	;; 2nd arg
+	mov rsi,r12
+	;; 3rd arg
+	mov rdx,[rsp]
+	
+	call r13
+
+	;; increment size of ImageSectionHeader
+	add r14,40
+	sub r15,1
+	cmp r15,0
+	jne __z__obj__run_through_coff_shdr3._b1
+	
+	;; mov rax,3
+	
 	;; pop rdx
 	;; pop rsi
 	;; pop rdi
@@ -31,11 +77,55 @@ run_through_coff_shdr3:
 	;; pop r15
 	;; pop r14
 	;; pop r13
-	pop r12
+._b2:
+	pop rdx
 	pop rbx
 	pop rbp
 	
 	ret
 	
-run_through_coff_symtable3:
+__z__obj__run_through_coff_symtable3:
+
+	push rbp
+	mov rbp,rsp
+	push rbx
+	push r12
+	push rdx
+	
+	;; 2nd(rsi) should be set on r13(which is one of registers kept on caller)
+	mov r13,rsi
+	;; get PointerToSymbolTable
+	mov dword eax,[rdi+0x8]
+	mov r15,rax
+	add r15,rdi
+	
+	;; get NumberOfSymbols
+	mov dword eax,[rdi+0xc]
+	mov r14,rax
+
+	;; get End of SymbolTable which is beginning of string table
+	mov rax,0x12
+	mul r14
+	mov r12, r15
+	add r12, rax
+._b1:
+
+	;; 1st arg
+	mov rdi,r15
+	;; 2nd arg
+	mov rsi,r12
+	;; 3rd arg
+	mov rdx,[rsp]
+	call r13
+
+	add r15,0x12
+	sub r14,1
+	cmp r14,0
+	jne __z__obj__run_through_coff_symtable3._b1
+
+	pop rdx
+	pop r12
+	pop rbx
+	pop rbp
+	
 	ret
--- zld.mk
+++ zld.mk
@@ -23,7 +23,7 @@ $(objdir)/zld.obj : src/tools/zld.c
 
 #####################################################
 
-$(sodir)/link.so.0 : $(objdir)/pack.obj $(objdir)/hashtable.obj $(objdir)/phdr.obj $(objdir)/static_data.obj $(objdir)/symbol.obj $(objdir)/object.obj $(objdir)/elf_callback.obj $(objdir)/callback.obj $(objdir)/reloc.obj $(objdir)/emit.obj $(objdir)/section.obj $(objdir)/elf_section.obj $(objdir)/dynamic.obj
+$(sodir)/link.so.0 : $(objdir)/pack.obj $(objdir)/hashtable.obj $(objdir)/phdr.obj $(objdir)/static_data.obj $(objdir)/elf_symbol.obj $(objdir)/object.obj $(objdir)/elf_callback.obj $(objdir)/callback.obj $(objdir)/elf_reloc.obj $(objdir)/elf_emit.obj $(objdir)/section.obj $(objdir)/elf_section.obj $(objdir)/dynamic.obj $(objdir)/coff_callback.obj $(objdir)/coff_reloc.obj $(objdir)/coff_import.obj $(objdir)/coff_export.obj $(objdir)/coff_section.obj $(objdir)/coff_symbol.obj $(objdir)/coff_emit.obj
 	$(cc) -w -shared $^ -o $@
 
 $(objdir)/callback.obj : $(coredir)/link/callback.asm
@@ -32,39 +32,71 @@ $(objdir)/callback.obj : $(coredir)/link/callback.asm
 $(objdir)/hashtable.obj : $(coredir)/link/hashtable.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
+###############################################################
+# ELF
+###############################################################
+
 $(objdir)/phdr.obj : $(coredir)/link/elf/phdr.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
 $(objdir)/static_data.obj : $(coredir)/link/elf/static_data.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
-$(objdir)/reloc.obj : $(coredir)/link/elf/reloc.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/emit.obj : $(coredir)/link/elf/emit.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/elf_callback.obj : $(coredir)/link/elf/callback.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/symbol.obj : $(coredir)/link/elf/symbol.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/object.obj : $(coredir)/link/object.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/elf_section.obj : $(coredir)/link/elf/section.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
-
-$(objdir)/section.obj : $(coredir)/link/section.c
+$(objdir)/elf_reloc.obj : $(coredir)/link/elf/reloc.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
 $(objdir)/dynamic.obj : $(coredir)/link/elf/dynamic.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
-$(objdir)/pack.obj : $(coredir)/pack/pack.c
+$(objdir)/elf_emit.obj : $(coredir)/link/elf/emit.c
 	$(cc) -fPIC -w -c $(header) $^ -o $@
 
+$(objdir)/elf_callback.obj : $(coredir)/link/elf/callback.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/elf_symbol.obj : $(coredir)/link/elf/symbol.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/elf_section.obj : $(coredir)/link/elf/section.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+###############################################################
+# COFF
+###############################################################
+
+$(objdir)/coff_callback.obj : $(coredir)/link/coff/callback.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_reloc.obj : $(coredir)/link/coff/reloc.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_import.obj : $(coredir)/link/coff/import.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_export.obj : $(coredir)/link/coff/export.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_section.obj : $(coredir)/link/coff/section.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_symbol.obj : $(coredir)/link/coff/symbol.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/coff_emit.obj : $(coredir)/link/coff/emit.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+###############################################################
+# common
+###############################################################
+
+$(objdir)/object.obj : $(coredir)/link/object.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/section.obj : $(coredir)/link/section.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
+
+$(objdir)/pack.obj : $(coredir)/pack/pack.c
+	$(cc) -fPIC -w -c $(header) $^ -o $@
 
 ##################################################################
 
@@ -96,14 +128,22 @@ $(objdir)/alloc.obj : src/core/memory/__alloc.c
 
 ##################################################################
 
-$(sodir)/objectfile.so.0 : $(objdir)/read_elf.obj $(objdir)/read_elf_callback.obj
-	$(cc) -w -shared $^ -o $@
+$(sodir)/objectfile.so.0 : $(objdir)/objformat.obj $(objdir)/read_elf.obj $(objdir)/read_coff.obj $(sodir)/os.so.0
+	$(cc) -w -nostdlib -shared $^ -o $@
+
+#  $(objdir)/read_elf_callback.obj
+
+$(objdir)/objformat.obj : $(coredir)/objectfile/__objformat.c
+	$(cc) -fPIC -w -nostdlib -c $(header) $^ -o $@
+
+$(objdir)/read_coff.obj : $(coredir)/objectfile/coff_pe/read_coff.asm
+	nasm -felf64 $^ -o $@
 
 $(objdir)/read_elf.obj : $(coredir)/objectfile/elf/read_elf.asm
 	nasm -felf64 $^ -o $@
 
 $(objdir)/read_elf_callback.obj : $(coredir)/objectfile/elf/read_elf_callback.c
-	$(cc) -fPIC -w -c $(header) $^ -o $@
+	$(cc) -fPIC -w -nostdlib -c $(header) $^ -o $@
 
 ##################################################################
 
